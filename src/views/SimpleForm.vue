c3b7def3f31f7f51d51c0d34388503d2b1a9e516
Sitnikov-Ryslan <sitnikov.ryslan1992@gmail.com>
2021-03-13 20:54:38 +0400

    submitForm

--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Create an event</h1>
-    <form>
+    <form @submit.prevent="sendForm">
       <BaseSelect
         :options="categories"
         v-model="event.category"
@@ -60,6 +60,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data () {
     return {
@@ -87,6 +89,17 @@ export default {
         { label: 'Yes', value: 1 },
         { label: 'No', value: 0 }
       ]
+    }
+  },
+  methods: {
+    sendForm () {
+      axios.post('https://my-json-server.typicode.com/Code-Pop/Vue-3-Forms/events', this.event)
+        .then(function (response) {
+          console.log('Response', response)
+        })
+        .catch(function (err) {
+          console.log('Error', err)
+        })
     }
   }
 }
